9fc5803ac2e06dc46633ddc719121e3b1f16889f
i-neeed-sleep <kammyl@nycstudents.net>
2025-11-13 13:21:16 -0500

    eeeeee

--- itstime.js
+++ itstime.js
@@ -1,3 +1,5 @@
+import "./style.css"
+
 let playlist = [{
         name: "You're Magical", 
         artists:["Elisa Rosselli", "Winx Club"],
@@ -5,12 +7,12 @@ let playlist = [{
         spotify: "https://open.spotify.com/track/7rROo4K5ZOCIkJ2uPtHWI4"
     },{
         name: "Never Gonna Give You Up", 
-        artist:["Rick Astley"],
+        artists:["Rick Astley"],
         genres: ["Pop"],
         spotify: "https://open.spotify.com/track/4uLU6hMCjMI75M1A2tKUQC"
     },{
         name: "All I Want For Christmas Is You", 
-        artist:["Mariah Carey"],
+        artists:["Mariah Carey"],
         genres: ["Christmas", "Holiday", "Pop"],
         spotify: "https://open.spotify.com/track/0bYg9bo50gSsH3LtXe2SQn"
 }]
@@ -19,20 +21,30 @@ let playlist = [{
 function show(song){
     const container = document.querySelector(".songs");
     container.insertAdjacentHTML("afterbegin", `
-        <div class="card"></div>
+        <div class="card">
+            <h2>${song.name} by ${song.artists}</h2>
+            <p>Genres: ${song.genres}</p>
+            <a href = "${song.spotify}">
+                <button class="msc"> Listen on Spotify </button>
+            </a>
+        </div>
     `)
+}
+
+function add(list){
+    for (i in playlist){
+        show(i);
+    }
 }
 
 function fltr(ask){
     for (i in playlist){
         if(i.name.lowercase() == ask.lowercase()){
-            show(i);
+            
+        } else{
+
         }
     }
 }
 
-function add(list){
 
-}
-
-// vite name is "stuff"
